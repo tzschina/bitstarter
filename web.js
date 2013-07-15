7411d4eb47ab0c367a5c40c522dc8bf91eb6600e
ZS Tan <tzschina@gmail.com>
2013-07-15 19:58:19 +0000

    read file web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer');
 
 var app = express.createServer(express.logger());
+var fileBuf = fs.readFileSync('index.html');
+var content = fileBuf.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World4!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
